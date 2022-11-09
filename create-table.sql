0344e6e09e753f95fd962de1513e36767b7bb7a5
Denna Mandela <denna.mandela13@gmail.com>
2022-11-10 00:42:37 +0700

    updating file database and table

---
+++ create-table.sql
@@ -0,0 +1,65 @@
+-- membuat table supplier --
+create table supplier(
+	id int not null auto_increment,
+    nama varchar (50) not null,
+    no_telp varchar (12) not null,
+    alamat text,
+    primary key(id)
+);
+
+-- membuat table category --
+create table category(
+	id int not null auto_increment,
+    category_name varchar (20) not null,
+    primary key(id)
+);
+
+-- membuat table customer --
+create table customer(
+	id int not null auto_increment,
+    nama_customer varchar (50) not null,
+    no_telp char (12) not null,
+    jenis_kelamin varchar (25) not null,
+    alamat text,
+    primary key(id)
+);
+
+-- membuat tabel product --
+create table product(
+	id int not null auto_increment,
+    nama_barang varchar (100) not null,
+    sku char (7) not null,
+    deskripsi text not null,
+    harga decimal (10,2) not null,
+    stok int not null,
+    supplier_id int not null,
+    category_id int not null,
+    primary key(id),
+    foreign key (supplier_id) references supplier(id),
+    foreign key (category_id) references category(id)
+);
+
+-- membuat table transaction --
+create table transaction(
+	id int not null auto_increment,
+    tanggal_transaksi date,
+    keterangan text,
+    customer_id int not null,
+    product_id int not null,
+    primary key(id),
+    foreign key (customer_id) references customer(id),
+    foreign key (product_id) references product(id)
+);
+
+-- create table payment--
+create table payment(
+	id int not null auto_increment,
+    transaction_id int not null,
+    pilih_bayar enum ('cod', 'bank') not null,
+    tanggal_bayar date,
+    total_bayar decimal (10,2) not null,
+    primary key(id),
+    foreign key (transaction_id) references transaction(id)
+);
+
+show tables;
